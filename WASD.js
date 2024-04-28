927e7f39ff6a7b146206760e1c69785ea7626e6c
Ibrahim-Turtle <166855157+Ibrahim-Turtle@users.noreply.github.com>
2024-04-28 14:51:17 +0200

    Add files via upload

---
+++ WASD.js
@@ -0,0 +1,12 @@
+document.addEventListener('keydown', function(event) {
+    var keyCode = event.keyCode;
+    if (keyCode === 87) { // W-toets
+        // Verplaats de auto naar voren
+    } else if (keyCode === 65) { // A-toets
+        // Verplaats de auto naar links
+    } else if (keyCode === 83) { // S-toets
+        // Verplaats de auto naar achteren
+    } else if (keyCode === 68) { // D-toets
+        // Verplaats de auto naar rechts
+    }
+});
